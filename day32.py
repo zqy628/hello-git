4835d39c68f326b7d2d6486a9055a0e6655a7794
zqy628 <191155395@qq.com>
2017-10-24 19:18:00 +0800

    update

--- day32.py
+++ day32.py
@@ -5,14 +5,32 @@
 
 第 0002 题: 将 0001 题生成的 200 个激活码（或者优惠券）保存到 MySQL 关系型数据库中。
 
-第 0003 题： 将 0001 题生成的 200 个激活码（或者优惠券）保存到 Redis 非关系型数据库中。
+第 0003 题： 将 0001 题生成的 200 个激活码（或者优惠券）保存到 mongoDB 非关系型数据库中。
 '''
 #激活码：8位数字大写字母随机组合
 import string
 import random
+import pymongo
+#保存在mongoDB中
+MS = {
+    # 'server': '10.80.94.84',
+    'server': 'localhost',
+    'port':37215,
+    'db': 'dailywork',
+    'collection': 'codes',
+    'user': 'zqy',
+    'password': '191155395'
+}
 letters = string.ascii_uppercase + string.digits
 def code():
     #无查重，需要时可以添加列表查重，while循环
     for i in xrange(200):
-        print ''.join([random.choice(letters) for i in xrange(8)]) #浓缩代码
+        code =  ''.join([random.choice(letters) for i in xrange(8)]) #浓缩代码
+        # 验证连接数据库
+        client = pymongo.MongoClient(MS['server'], MS['port'])
+        MS_database = client[MS['db']]
+        MS_database.authenticate(MS['user'], MS['password'], source='admin')
+        if not MS_database['codes'].find_one({"code": code}):  # 新建站点表,增加查重
+            MS_database['codes'].insert_one(
+                {"code": code})
 code()
